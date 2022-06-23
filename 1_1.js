b10fa7ec0952ada96fe967909d0c78442fc89dca
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-06-23 16:33:54 +0300

    made changes in

--- 1_1.js
+++ 1_1.js
@@ -1,3 +1,10 @@
 console.log('hello, World!');
 
 console.log('hello, friends');
+const userAge = 35;
+if(userAge >= 18) {
+    console.log('Вы совершеннолетний');
+}
+else {
+    console.log('Вы не совершеннолеиний');
+}
