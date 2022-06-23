e055fe279e97839ef5fadde07508825eb928b969
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-06-23 16:29:42 +0300

    just commit

--- 1_1.js
+++ 1_1.js
@@ -1,3 +1,11 @@
 console.log('hello, World!');
 
 console.log('hello, friends');
+const age = 20;
+if (age>18) {
+    console.log('Вы совершеннолетний');
+}
+else {
+    console.log('Вы НЕ совершеннолетний');
+}
+console.log('hello');
